11c8ec44c51f690e5d4c2d1a71020ad8f9bca145
irwansyah-dev <irwansyah.dev96@gmail.com>
2025-08-26 09:32:51 +0700

    add config and update table

--- mybook_ms_read.sql
+++ mybook_ms_read.sql
@@ -7,10 +7,8 @@
 CREATE TABLE `tb_read_book` (
   `id` varchar(36) NOT NULL,
   `date_of_read` bigint NOT NULL,
-  `page_of_read` int DEFAULT NULL,
-  `issbn` varchar(15) DEFAULT NOT NULL,
+  `page_of_read` int NOT NULL,
+  `issbn` varchar(15) NOT NULL,
   `note` text,
-  PRIMARY KEY (`id`),
-  KEY `FK_book_issbn` (`issbn`),
-  CONSTRAINT `FK_book_issbn` FOREIGN KEY (`issbn`) REFERENCES `tb_book` (`issbn`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
